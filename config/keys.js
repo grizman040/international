9a3ec52329de1cb7d07465fb12c01ead1fd68a2b
grizman040 <grizman040@gmail.com>
2020-09-03 17:35:50 +0200

    added like button, Posted Time

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//     MONGOURI: "mongodb+srv://devco:JJKcbdu1OuZiEMAh@cluster0.kx6nq.mongodb.net/test?retryWrites=true&w=majority",
-//     JWT_SECRET:"fskjnkjn4bnj43hb"
-// }
-
-//https://cloudinary.com/:  !NcvyvkMPD5FSi_
-
-
-// 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
 
 if(process.env.NODE_ENV==='production'){
     module.exports = require('./prod')
